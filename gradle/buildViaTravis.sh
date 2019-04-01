779e2ac1cf85e04d89efab67ad8320cffee99ad0
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-04-01 16:55:03 -0600

    plugin-plugin 11.0.1

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   ./gradlew build $SWITCHES
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -Prelease.travisci=true build snapshot $SWITCHES
+  ./gradlew -Prelease.travisci=true build  $SWITCHES
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
