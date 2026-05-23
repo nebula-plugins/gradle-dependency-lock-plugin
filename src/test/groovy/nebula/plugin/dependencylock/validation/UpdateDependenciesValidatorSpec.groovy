ec1bc921688b42365c8e1215d7481dff814648f6
Aubrey Chipman <achipman@netflix.com>
2026-05-23 15:16:54 -0400

    format: reformat before changes (npm-groovy-lint)

--- src/test/groovy/nebula/plugin/dependencylock/validation/UpdateDependenciesValidatorSpec.groovy
+++ src/test/groovy/nebula/plugin/dependencylock/validation/UpdateDependenciesValidatorSpec.groovy
@@ -5,6 +5,7 @@ import nebula.plugin.dependencylock.exceptions.DependencyLockException
 import nebula.plugin.dependencylock.FreshProjectSpec
 
 class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
+
     private static final HashMap<Object, Object> emptyMap = new HashMap<Object, Object>()
     private static final String pluginName = 'com.netflix.nebula.dependency-lock'
     DependencyLockExtension extension
@@ -16,8 +17,8 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
 
     def 'should not fail if valid coordinates'() {
         when:
-        Set<String> modules = ["netflix:my-module", "netflix:my-module-2"]
-        UpdateDependenciesValidator.validate(modules, emptyMap, true, false, 
+        Set<String> modules = ['netflix:my-module', 'netflix:my-module-2']
+        UpdateDependenciesValidator.validate(modules, emptyMap, true, false,
             extension.updateDependenciesFailOnInvalidCoordinates.get(),
             extension.updateDependenciesFailOnSimultaneousTaskUsage.get(),
             extension.updateDependenciesFailOnNonSpecifiedDependenciesToUpdate.get())
@@ -28,7 +29,7 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
 
     def 'should fail if invalid coordinates'() {
         when:
-        Set<String> modules = ["netflix", "netflix:my-module:1.+", "netflix:my-module-2:latest.release", "netflix:my-module;2:latest.release"]
+        Set<String> modules = ['netflix', 'netflix:my-module:1.+', 'netflix:my-module-2:latest.release', 'netflix:my-module;2:latest.release']
         UpdateDependenciesValidator.validate(modules, emptyMap, true, false,
             extension.updateDependenciesFailOnInvalidCoordinates.get(),
             extension.updateDependenciesFailOnSimultaneousTaskUsage.get(),
@@ -36,17 +37,17 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
 
         then:
         def exception = thrown(DependencyLockException)
-        exception.message.contains("netflix does not contain groupId:module. Only has one element")
-        exception.message.contains("netflix:my-module:1.+ contains more elements than groupId:module. Version and classifiers are not supported")
-        exception.message.contains("netflix:my-module-2:latest.release contains more elements than groupId:module. Version and classifiers are not supported")
-        exception.message.contains("netflix:my-module;2:latest.release contains more elements than groupId:module. Version and classifiers are not supported")
-        exception.message.contains("netflix:my-module;2:latest.release contains ; which is invalid")
+        exception.message.contains('netflix does not contain groupId:module. Only has one element')
+        exception.message.contains('netflix:my-module:1.+ contains more elements than groupId:module. Version and classifiers are not supported')
+        exception.message.contains('netflix:my-module-2:latest.release contains more elements than groupId:module. Version and classifiers are not supported')
+        exception.message.contains('netflix:my-module;2:latest.release contains more elements than groupId:module. Version and classifiers are not supported')
+        exception.message.contains('netflix:my-module;2:latest.release contains ; which is invalid')
     }
 
     def 'should not fail if invalid coordinates but disabled validation'() {
         when:
         project.ext.set('dependencyLock.updateDependenciesFailOnInvalidCoordinates', false)
-        Set<String> modules = ["netflix", "netflix:my-module:1.+", "netflix:my-module-2:latest.release", "netflix:my-module;2:latest.release"]
+        Set<String> modules = ['netflix', 'netflix:my-module:1.+', 'netflix:my-module-2:latest.release', 'netflix:my-module;2:latest.release']
         // Use findProperty to check both gradle properties and ext (check for null, not truthiness)
         def validateCoordinatesProp = project.findProperty('dependencyLock.updateDependenciesFailOnInvalidCoordinates')
         def validateCoordinates = validateCoordinatesProp != null ? validateCoordinatesProp as boolean : extension.updateDependenciesFailOnInvalidCoordinates.get()
@@ -73,7 +74,7 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
 
     def 'should not fail when calling one locking generation command - updateLock'() {
         when:
-        Set<String> modules = ["netflix:my-module", "netflix:my-module-2"]
+        Set<String> modules = ['netflix:my-module', 'netflix:my-module-2']
         UpdateDependenciesValidator.validate(modules, emptyMap, true, false,
             extension.updateDependenciesFailOnInvalidCoordinates.get(),
             extension.updateDependenciesFailOnSimultaneousTaskUsage.get(),
@@ -85,7 +86,7 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
 
     def 'should fail when calling generateLock and updateLock together'() {
         when:
-        Set<String> modules = ["netflix:my-module", "netflix:my-module-2"]
+        Set<String> modules = ['netflix:my-module', 'netflix:my-module-2']
         UpdateDependenciesValidator.validate(modules, emptyMap, true, true,
             extension.updateDependenciesFailOnInvalidCoordinates.get(),
             extension.updateDependenciesFailOnSimultaneousTaskUsage.get(),
@@ -93,14 +94,14 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
 
         then:
         def exception = thrown(DependencyLockException)
-        exception.message.contains("Using `generateLock` and `updateLock` in the same build will result in re-resolving all locked dependencies.")
-        exception.message.contains("Please invoke only one of these tasks at a time.")
+        exception.message.contains('Using `generateLock` and `updateLock` in the same build will result in re-resolving all locked dependencies.')
+        exception.message.contains('Please invoke only one of these tasks at a time.')
     }
 
     def 'should not fail when calling generateLock and updateLock together but disabled validation'() {
         when:
         project.ext.set('dependencyLock.updateDependenciesFailOnSimultaneousTaskUsage', false)
-        Set<String> modules = ["netflix:my-module", "netflix:my-module-2"]
+        Set<String> modules = ['netflix:my-module', 'netflix:my-module-2']
         // Use findProperty to check both gradle properties and ext (check for null, not truthiness)
         def validateSimultaneousProp = project.findProperty('dependencyLock.updateDependenciesFailOnSimultaneousTaskUsage')
         def validateSimultaneous = validateSimultaneousProp != null ? validateSimultaneousProp as boolean : extension.updateDependenciesFailOnSimultaneousTaskUsage.get()
@@ -117,8 +118,8 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
         when:
         Set<String> modules = []
         Map<String, String> overrides = new HashMap<>()
-        overrides.put("netflix:my-module", "1.0.0")
-        overrides.put("netflix:my-module-2", "1.0.0")
+        overrides.put('netflix:my-module', '1.0.0')
+        overrides.put('netflix:my-module-2', '1.0.0')
         UpdateDependenciesValidator.validate(modules, overrides, true, false,
             extension.updateDependenciesFailOnInvalidCoordinates.get(),
             extension.updateDependenciesFailOnSimultaneousTaskUsage.get(),
@@ -138,7 +139,7 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
 
         then:
         def exception = thrown(DependencyLockException)
-        exception.message.contains("Please specify dependencies to update")
+        exception.message.contains('Please specify dependencies to update')
     }
 
     def 'should not fail when no modules to update and no overrides are passed in but disabled validation'() {
@@ -156,4 +157,5 @@ class UpdateDependenciesValidatorSpec extends FreshProjectSpec {
         then:
         notThrown(DependencyLockException)
     }
+
 }
